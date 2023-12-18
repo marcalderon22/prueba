551cd423a13d2d63bb68ee630d3e6a2707414af8
marcalderon22 <mcalderon@musclepoints.com>
2023-12-18 11:50:40 -0600

    fix: error env variable

--- src/main.py
+++ src/main.py
@@ -3,16 +3,20 @@ import importlib
 import os
 
 test_param = os.environ.get('TEST_PARAM', 'default_value')
+address = os.environ.get('TEST_ADDRESS', 'default_value')
+
 def process_address():
     dir = "scripts"
 
     try:
-        script_module = importlib.import_module(f"{dir}.{test_param}")
-        response = getattr(script_module, "response", None)()
+        script_module = importlib.import_module(f"{dir}.{address}")
+        response = getattr(script_module, "response", None)(test_param)
         print(json.dumps(response, indent = 2))
     except ImportError as e:
         print(f"Error: {e}")
     except Exception as e:
         print(f"Error: {e}")
+
+process_address()
 
 
