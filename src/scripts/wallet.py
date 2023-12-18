9055eaa6f7f6fa8ddc61b65ddcf056e207e3b956
marcalderon22 <mcalderon@musclepoints.com>
2023-12-18 11:55:23 -0600

    update: .yml

--- src/scripts/wallet.py
+++ src/scripts/wallet.py
@@ -1,5 +1,5 @@
 def response(key):
     if key ==  "1234PYTHON5678":
-        return {"message": "Hello from wallet.py"}
+        return {"message": "Hello from Script for W4LL3T"}
     else:
         return {"error": "Invalid key"}
